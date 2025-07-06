4cf2317e77233b6df37e60a62bb37cbd4bddf471
mek-12 <mek.krbg04@gmail.com>
2025-07-06 16:38:14 +0300

    repository edited

--- Navend.Core/Data/IRepository.cs
+++ Navend.Core/Data/IRepository.cs
@@ -2,7 +2,8 @@ using System.Linq.Expressions;
 
 namespace Navend.Core.Data;
 
-public interface IRepository<TEntity, in TKey>: IRepository where TEntity: class, IEntity<TKey> {
+public interface IRepository<TEntity, in TKey>: IRepository where TEntity: class, IEntity<TKey> 
+{
     Task<TEntity?> GetAsync(TKey id);
     Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate);
     Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? predicate = null);
